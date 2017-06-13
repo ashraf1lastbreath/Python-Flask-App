9f2520debcc5b32264d1f9ab48a603532b5af9ee
Ashraf1lastbreath <ashraf.mchoudhury@gmail.com>
2017-06-13 05:37:01 +0530

    updated with  user authentication system to create sign in to the app

---
+++ app.py
@@ -0,0 +1,78 @@
+from flask import Flask, render_template, flash, redirect, url_for, session, logging, request
+from products import products
+from flask_pymongo import PyMongo
+import pymongo
+import sys
+from pymongo import MongoClient
+from ConfigParser import SafeConfigParser
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
+
+#config PyMongo Db
+parser = SafeConfigParser( )
+parser.read('config.ini')
+connection = MongoClient(parser.get('mongo_server', 'mongo_url'))
+
+user_collection = connection.myflaskapp.users
+app = Flask(__name__)
+
+#initialize PyMongo
+mongo = PyMongo(app)
+Products = products( )
+
+@app.route( '/')
+def index( ):
+    return render_template('home.html')
+
+@app.route( '/about')
+def about( ):
+    return render_template('about.html')
+
+@app.route( '/contact')
+def contact( ):
+    return render_template('contact.html')
+
+@app.route('/products')
+def products( ):
+    return render_template('products.html' , products = Products)
+
+@app.route('/product/<string:id>/')
+def product( id):
+    return render_template('product.html' , id = id)
+
+# Form Class with WTForms
+class RegisterForm(Form):
+    name = StringField('Name', [validators.Length(min = 1, max = 50)])
+    username = StringField('Email', [validators.Length(min = 4, max = 50)])
+    email = StringField('Username', [validators.Length(min = 6, max = 50)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm',message = 'Passwords do not match')
+        ])
+    confirm = PasswordField('Confirm Password')
+
+        
+#route for register form
+@app.route('/register', methods = ['GET', 'POST'])
+def register():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        name = form.name.data
+        email = form.email.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+
+        #post into db
+        user_collection.insert_one({'name': name, 'email':email, 'username':username, 'password': password})
+        sys.stdout.flush()
+        connection.close()
+        flash("You have successfully registered !!", 'success')
+        return redirect(url_for('register'))
+
+    return render_template('register.html', form = form)
+
+
+
+if __name__ =='__main__':
+    app.secret_key = 'secret123'
+    app.run(debug= True)
---
+++ products.py
@@ -0,0 +1,28 @@
+def products( ):
+  products = [
+      {
+          'id' :1,
+          'item' : "AmazonBasics In-Ear Headphones (Blue)",
+          'host' : "www.amazon.in",
+          'prod' : "AmazonBasics In-Ear Headphones (Blue)",
+          'image' : "http://ecx.images-amazon.com/images/I/51EOx4I8JBL._SL1189_.jpg",
+          'prodid' : "B01F2O1CFK",
+          'desc' : "In-ear headphones provide comfort, quality, and style Delivers dynamic, crystal-clear sound and deep powerful bass; soft silicone, ergonomic earbuds block outside noise Silver-plated 3.5 mm audio jack creates optimal sound connection to audio devices 4-foot cord offers flexibility and a comfortable length; can be threaded through a jacket or bag Clip included for attaching cord to clothing Compatible with any iPod, iPhone, iPad, Android device, mp3 player, CD player, portable DVD player, radio, or laptop computer Warranty: 1 Year Limited Warranty",
+          'currency' : "INR",
+          'url' : "http://www.amazon.in/AmazonBasics-EB01BU-In-Ear-Headphones-Blue/dp/B01F2O1CFK/ref=gbph_img_s-3_9b4f_7b777419?smid=AT95IG9ONZD7S&pf_rd_p=ba35bde6-038b-44ac-8edb-804316fe9b4f&pf_rd_s=slot-3&pf_rd_t=701&pf_rd_i=gb_main&pf_rd_m=A1VBAL9TL5WCBF&pf_rd_r=1BCBZXCKFTEDF12H3QG0",
+          'price' : "399"
+        },
+        {
+          'id':2,
+          'item' : "Dektron 81.3 cm (32 inches) DK3217FHD Full HD LED TV",
+          'host' : "www.amazon.in",
+          'prod' : "Dektron 81.3 cm (32 inches) DK3217FHD Full HD LED TV",
+          'image' : "http://ecx.images-amazon.com/images/I/51EOx4I8JBL._SL1189_.jpg",
+          'prodid' : "B06XSNP7TQ",
+          'desc' : "Display: 1920 X 1080 (Full HD), A+ Grade LED Panel, 16.7 Million Colours Connectivity: HDMI * 2, USB * 2, VGA * 1, AV in, ANT, PC Audio Installation: Wall Mount Kit & Table stand with an installation guide is provided for easy installation Warranty Information: 1 Year Dektron Warranty Service: We provide service on 19,000 Pincodes, To avail service under warranty please call Dektron Customer Support on +91-9871996644 and provide the Product's Model no. & Order no. mentioned on the invoice",
+          'currency' : "INR",
+          'url' : "http://www.amazon.in/dp/B06XSNP7TQ?psc=1",
+          'price' : "12499"
+        }
+      ]
+  return products
